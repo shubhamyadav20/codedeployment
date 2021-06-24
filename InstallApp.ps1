7526883f9cbafac9745c8197168b8627da25d0dd
shubhamyadav20 <40170631+shubhamyadav20@users.noreply.github.com>
2021-06-24 07:22:38 +0530

    Update InstallApp.ps1

--- InstallApp.ps1
+++ InstallApp.ps1
@@ -9,4 +9,4 @@ sl C:\website-data
 
 # Point IIS wwwroot of the published folder. CodeDeploy uses 32 bit version of PowerShell.
 # To make use the IIS PowerShell CmdLets we need call the 64 bit version of PowerShell.
-C:\Windows\SysNative\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value c:\website-data\publish}
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command {Import-Module WebAdministration; Set-ItemProperty 'IIS:\sites\Default Web Site' -Name physicalPath -Value c:\website-data\publish}
